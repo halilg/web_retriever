af5240920578d5358dc1900e9fb87d7037b3ed1e
Halil Gamsizkan <halilgamsizkan@Darth-Halils-Evil-Macbook.local>
2014-05-28 21:26:45 +0300

    recreate all option for make_all_html.zsh

--- make_all_html.zsh
+++ make_all_html.zsh
@@ -1,8 +1,11 @@
 #!/usr/bin/env zsh
 
+# -R flag forces recreation of HTML files
+
 RSSDIR=./RSS
 HTMLDIR=./arXiv
 mkdir -p $HTMLDIR
+OPT=$1
 
 for xml in $RSSDIR/*; do
     xmlname=$(basename $xml)
@@ -11,8 +14,8 @@ for xml in $RSSDIR/*; do
     targetdir=$HTMLDIR/$feed
     htmlfile=$targetdir/`basename $xml`.html
     htmlfile=`echo $htmlfile | sed s/.rss.gz//`
-    if [ -s $htmlfile ]; then
-        #echo "skipping" $htmlfile
+    if [[ -s $htmlfile && $OPT != -R ]]; then
+        echo "skipping" $htmlfile
     else
         mkdir -p $targetdir
         ./arXivHTML.py $xml $htmlfile
